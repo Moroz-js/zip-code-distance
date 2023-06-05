27f5317a38d56b75ae3723677de857cbd560686f
Vladimir <lisoff.vol@yandex.ru>
2023-06-05 15:35:34 +0300

    array zip

---
+++ app.js
@@ -0,0 +1,32 @@
+//express server import
+const express = require('express');
+
+//body parser
+const bodyParser = require('body-parser');
+
+//CORS
+var cors = require('cors')
+
+//initialize express server app
+const app = express();
+
+//cors middleware
+app.use(cors());
+
+
+
+//set port for server
+app.set('port', (process.env.PORT || 3002));
+
+//Routes
+const distance = require('./api/getDistance');
+const info = require('./api/getInfo');
+const radius = require('./api/getRadius');
+
+app.use('/api/getDistance', distance);
+app.use('/api/getInfo', info);
+app.use('/api/getRadius', radius);
+
+app.listen(app.get('port'), () => {
+    console.log(`Server is running on port ${app.get('port')}`);
+})
